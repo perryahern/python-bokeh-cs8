309d8cb5f41444a4b4b35d768cc03fed7cb56141
perryahern <pahern@gmail.com>
2018-06-20 18:59:27 -0400

    Completed day 1 assignment plus stretch

--- src/draw.py
+++ src/draw.py
@@ -2,35 +2,68 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, ColumnDataSource, Range1d, LabelSet, Label
 from bokeh.palettes import Spectral8
 
-N = 8
+from graph import *
+
+graph_data = Graph()
+graph_data.debug_create_test_data()
+
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+color_list = []
+for vertex in graph_data.vertexes:
+  color_list.append(vertex.color)
+
+plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.data_source.add(color_list, 'color')
+graph.node_renderer.glyph = Oval(height=30, width=38, fill_color='color')
 
-graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+# this is drawing the edges from start to end
+graph.edge_renderer.data_source.data = dict(start=[], end=[])
+    # start=[i for i, vertex in enumerate(graph_data.vertexes)],  # this is why all edges start from first vertex, a list that has to do with starting points
+    # end=[edge for vertex in graph_data.vertexes.edges) # a list of some kind that has to do with ending points
+    # end=[1, 2, 3, 4]) # a list of some kind that has to do with ending points
+
+for vertex in graph_data.vertexes:
+  if len(vertex.edges) > 0:
+    for edge in vertex.edges:
+      start = graph_data.vertexes.index(vertex)
+      graph.edge_renderer.data_source.data['start'].append(start)
+
+      end = graph_data.vertexes.index(edge.destination)
+      graph.edge_renderer.data_source.data['end'].append(end)
+
+
+print(graph.edge_renderer.data_source.data)
 
 ### start of layout code
 # looks like this is setting positions of the vertexes
-circ = [i*2*math.pi/8 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+circ = [i*2*math.pi/N for i in node_indices]
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
+
+source = ColumnDataSource(data=dict(x_pos=x,
+                                    y_pos=y,
+                                    names=['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7']))
+
+labels = LabelSet(x='x_pos', y='y_pos', text='names', level='glyph',
+              x_offset=-10, y_offset=-10, source=source, render_mode='canvas')
+
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
+
+plot.add_layout(labels)
 
 output_file('graph.html')
 show(plot)
--- src/graph.py
+++ src/graph.py
@@ -16,9 +16,30 @@ class Graph:
   def debug_create_test_data(self):
     debug_vertex_1 = Vertex('t1', x=40, y=40)
     debug_vertex_2 = Vertex('t2', x=140, y=140)
+    debug_vertex_3 = Vertex('t3', x=220, y=250)
+    debug_vertex_4 = Vertex('t4', x=150, y=300)
+    debug_vertex_5 = Vertex('t5', x=250, y=350)
+    debug_vertex_6 = Vertex('t6', x=350, y=275)
+    debug_vertex_7 = Vertex('t6', x=350, y=400)
+
     # print(debug_vertex_1.pos['x'])
 
     debug_edge_1 = Edge(debug_vertex_2)
     debug_vertex_1.edges.append(debug_edge_1)
 
-    self.vertexes.extend([debug_vertex_1, debug_vertex_2])
+    debug_edge_2 = Edge(debug_vertex_3)
+    debug_vertex_2.edges.append(debug_edge_2)
+
+    debug_edge_3 = Edge(debug_vertex_4)
+    debug_vertex_3.edges.append(debug_edge_3)
+
+    debug_edge_4 = Edge(debug_vertex_5)
+    debug_vertex_4.edges.append(debug_edge_4)
+
+    debug_edge_5 = Edge(debug_vertex_6)
+    debug_vertex_5.edges.append(debug_edge_5)
+
+    debug_edge_5 = Edge(debug_vertex_7)
+    debug_vertex_5.edges.append(debug_edge_5)
+    
+    self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4, debug_vertex_5, debug_vertex_6, debug_vertex_7])

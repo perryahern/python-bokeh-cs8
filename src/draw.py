cdc0e1a813ab9c4f36a784af80610303e6eb7980
perryahern <pahern@gmail.com>
2018-06-21 19:04:49 -0400

    Get connected verts and randomize working

--- src/draw.py
+++ src/draw.py
@@ -7,30 +7,36 @@ from bokeh.palettes import Spectral8
 
 from graph import *
 
+WIDTH = 640   # TODO: Currently gaph renders square, scaled to these numbers
+HEIGHT = 480  # TODO: Check for solutions (possible: https://stackoverflow.com/questions/21980662/how-to-change-size-of-bokeh-figure)
+CIRCLE_SIZE = 30
+
+
 graph_data = Graph()
-graph_data.create_test_data()
+# graph_data.create_test_data()
+graph_data.randomize(4, 3, 150, 0.6)
 
 N = len(graph_data.vertexes)
 node_indices = list(range(N))
+graph_data.get_connected_components()
 
 color_list = []
 for vertex in graph_data.vertexes:
   color_list.append(vertex.color)
 
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+plot = figure(title='Graph Layout Demonstration', x_range=(0, WIDTH), y_range=(0, HEIGHT),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Circle(size=30, fill_color='color')
+graph.node_renderer.glyph = Circle(size=CIRCLE_SIZE, fill_color='color')
 
 # this is drawing the edges from start to end
 graph.edge_renderer.data_source.data = dict(start=[], end=[])
 
 for vertex in graph_data.vertexes:
-  # if len(vertex.edges) > 0:             # not needed?
   for edge in vertex.edges:
     start = graph_data.vertexes.index(vertex)
     graph.edge_renderer.data_source.data['start'].append(start)
@@ -55,7 +61,6 @@ graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
-
 
 output_file('graph.html')
 show(plot)
--- src/graph.py
+++ src/graph.py
@@ -1,11 +1,14 @@
+from random import randint
+import random
+
 class Edge:
   def __init__(self, destination):
     self.destination = destination
 
 class Vertex:
-  def __init__(self, value, color, **pos):   #TODO: test default arguments
+  def __init__(self, value, **pos):   #TODO: test default arguments
     self.value = value
-    self.color = color
+    self.color = 'white'
     self.pos = pos
     self.edges = []
 
@@ -14,13 +17,18 @@ class Graph:
     self.vertexes = []
 
   def create_test_data(self):
-    vertex_1 = Vertex('V1', 'red', x=40, y=40)
-    vertex_2 = Vertex('V2', 'blue', x=140, y=140)
-    vertex_3 = Vertex('V3', 'green', x=220, y=250)
-    vertex_4 = Vertex('V4', 'yellow', x=150, y=300)
-    vertex_5 = Vertex('V5', 'purple', x=250, y=350)
-    vertex_6 = Vertex('V6', 'cyan', x=350, y=275)
-    vertex_7 = Vertex('V7', 'teal', x=350, y=400)
+    vertex_1 = Vertex('t1', x=40, y=40)
+    vertex_2 = Vertex('t2', x=140, y=140)
+    vertex_3 = Vertex('t3', x=220, y=250)
+    vertex_4 = Vertex('t4', x=150, y=300)
+    vertex_5 = Vertex('t5', x=250, y=350)
+    vertex_6 = Vertex('t6', x=350, y=275)
+    vertex_7 = Vertex('t7', x=350, y=400)
+    vertex_8 = Vertex('t8', x=400, y=425)
+    vertex_9 = Vertex('t9', x=425, y=375)
+    vertex_10 = Vertex('t10', x=450, y=325)
+    vertex_11 = Vertex('t11', x=300, y=100)
+    vertex_12 = Vertex('t12', x=350, y=150)
 
     vertex_1.edges.append(Edge(vertex_2))
 
@@ -33,5 +41,79 @@ class Graph:
 
     vertex_5.edges.append(Edge(vertex_6))
     vertex_5.edges.append(Edge(vertex_7))
+
+    vertex_8.edges.append(Edge(vertex_9))
+    vertex_9.edges.append(Edge(vertex_10))
+
+    vertex_11.edges.append(Edge(vertex_12))
     
-    self.vertexes.extend([vertex_1, vertex_2, vertex_3, vertex_4, vertex_5, vertex_6, vertex_7])
+    
+    self.vertexes.extend([vertex_1, vertex_2, vertex_3, vertex_4, vertex_5,
+                          vertex_6, vertex_7, vertex_8, vertex_9, vertex_10, vertex_11, vertex_12])
+
+  def randomize(self, width, height, pxBox, probability=0.6):
+    def connectVerts(v0, v1):
+      v0.edges.append(Edge(v1))
+      v1.edges.append(Edge(v0))
+  
+    count = 0
+
+    grid = []
+    for y in range(height):
+      row = []
+      for x in range(width):
+        value = 't' + str(count)
+        count += 1
+        v = Vertex(value)
+        row.append(v)
+      grid.append(row)
+    
+    for y in range(height):
+      for x in range(width):
+        #connect down
+        if y < height -1:
+          if randint(0, 1) < probability:
+            connectVerts(grid[y][x], grid[y+1][x])
+
+        #connect right
+        if x < width -1:
+          if randint(0, 1) < probability:
+            connectVerts(grid[y][x], grid[y][x+1])
+  
+    boxBuffer = 0.5
+    boxInner = pxBox * boxBuffer
+    boxInnerOffset = (pxBox - boxInner) / 2
+
+    for y in range(height):
+      for x in range(width):
+        grid[y][x].pos = {
+          'x': (x * pxBox + boxInnerOffset + randint(0, 1) * boxInner),
+          'y': (y * pxBox + boxInnerOffset + randint(0, 1) * boxInner) 
+        }
+    
+    for y in range(height):
+      for x in range(width):
+        self.vertexes.append(grid[y][x])
+
+  
+  def bfs(self, start):
+    queue = [start]
+    found = [start]
+
+    start.color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+
+    while len(queue) > 0:
+      v = queue[0]
+      for edge in v.edges:
+        if edge.destination not in found:
+          found.append(edge.destination)
+          queue.append(edge.destination)
+          edge.destination.color = start.color
+      queue.pop(0)  # TODO: Look at collections.dequeue
+    return found
+
+  def get_connected_components(self):
+    searched = []
+    for vertex in self.vertexes:
+      if vertex not in searched:
+        searched = searched + [vertex] + self.bfs(vertex)
